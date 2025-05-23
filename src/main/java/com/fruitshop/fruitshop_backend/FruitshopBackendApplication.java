314cff25716727c140c89a750e9508ea45d43e9d
leeseokchan <dltrjcks97@gmail.com>
2025-05-23 11:24:28 +0900

    프로젝트 시작

---
+++ src/main/java/com/fruitshop/fruitshop_backend/FruitshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fruitshop.fruitshop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitshopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fruitshop/fruitshop_backend/FruitshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fruitshop.fruitshop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
