aac5df854f5b14d9c8d70a77d1d94fab65389286
leeseokchan <dltrjcks97@gmail.com>
2025-06-16 17:18:33 +0900

    [security] 관리자 로그인페이지 수정 및 implement 설정

--- src/main/java/com/fruitshop/fruitshop_backend/service/CustomUserDetailService.java
+++ src/main/java/com/fruitshop/fruitshop_backend/service/CustomUserDetailService.java
@@ -2,19 +2,21 @@ package com.fruitshop.fruitshop_backend.service;
 
 import com.fruitshop.fruitshop_backend.dto.UserDto;
 import com.fruitshop.fruitshop_backend.mapper.UserMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailService {
+public class CustomUserDetailService implements UserDetailsService {
 
     private final UserMapper userMapper;
 
